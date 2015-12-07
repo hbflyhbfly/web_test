b3f608e07de776c1f63a06fbe8d890c9a04f8e76
Zhou Fei <hbflyhbfly@gmail.com>
2015-12-07 18:02:30 +0800

    reply

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,6 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var wechat = require('wechat');
+
+var config = {
+  token:'syuuhi',
+  appid:'wxb0864d52be23eaaa',
+  encodingAESKey:'744UyZhHQsHsgfogSND5KGpTmzy6d3IZSTWvI22bu5O'
+
+}
+
 var app = express();
 
 // view engine setup
@@ -23,6 +32,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//app.use(app.query());
+app.use('/wechat',wechat(config,function(req,res,next){
+  var message = req.weixin;
+  if(message.fromUsername == 'diaosi'){
+    res.reply('hehe');
+  }else if (message.fromUsername == 'text'){
+    res.replay({
+      content:"说啥捏",
+      type:'text'
+    });
+  }else if (message.fromUsername == 'hehe'){
+    res.reply({
+      type:'music',
+      content:{
+        title:"来听首歌吧",
+        description:"晴天",
+        musicUrl:"http://y.qq.com/#type=song&mid=0039MnYb0qxYhV",
+        hdMusiUrl:"http://y.qq.com/#type=song&mid=0039MnYb0qxYhV",
+        thumbMediaId:"0039MnYb0qxYhV"
+      }
+    });
+  }else{
+    res.reply([
+      {
+        title:"范美丽,你要等我",
+        description:"这是账房大娘和程序猿的对话",
+        picurl:"http://nodeapi.cloudfoundry.com/qrcode.jpg",
+        url:"http://nodeapi.cloudfoundry.com/"
+      }
+    ]);
+  }
+}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -53,6 +95,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
 
 
 module.exports = app;
