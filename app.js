25d69052098d18a9fbc757495de0e2081468e17b
Zhou Fei <hbflyhbfly@gmail.com>
2015-12-28 02:30:26 +0800

    dd

--- app.js
+++ app.js
@@ -69,29 +69,31 @@ function validateToken(req,res){
   res.end();
 }
 
-app.use('/wechat', wechat('syuuhi', function(message, req, res) {
-  res.reply('你好！');
-}));
+//app.use('/wechat', wechat('syuuhi', function(req, res,next) {
+//  res.reply('你好！');
+//}));
 app.get('/wechat',validateToken);
 
-//app.use('/wechat', wechat('syuuhi', function (req, res, next) {
-//  // 微信输入信息都在req.weixin上
-//  var message = req.weixin;
-//  console.log(message);
-//  if((message.MsgType == 'event') && (message.Event == 'subscribe'))
-//  {
-//    var refillStr = "<a href=\"http://your_IP/weixin/refill?weixinId=" + message.fromUsername + "\">1. 点击记录团队充值</a>"
-//
-//    var consumeStr = "<a href=\"http://your_IP/weixin/consume?weixinId=" + message.fromUsername + "\">2. 点击记录团队消费</a>"
-//    var deleteStr = "<a href=\"http://your_IP/weixin/delete?weixinId=" + message.fromUsername + "\">3. 点击回退记录</a>"
-//    var historyStr = "<a href=\"http://your_IP/weixin/history?weixinId=" + message.fromUsername + "\">4. 点击查询历史记录</a>"
-//
-//    var emptyStr = "          ";
-//    var replyStr = "感谢你的关注！" + "\n"+ emptyStr + "\n" + refillStr + "\n"+ emptyStr + "\n" + consumeStr
-//        + "\n"+ emptyStr + "\n" + deleteStr + "\n"+ emptyStr + "\n" + historyStr;
-//    res.reply(replyStr);
-//  }
-//}));
+app.use('/wechat', wechat('syuuhi', function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  console.log(message);
+  if((message.MsgType == 'event') && (message.Event == 'subscribe'))
+  {
+    var refillStr = "<a href=\"http://your_IP/weixin/refill?weixinId=" + message.fromUsername + "\">1. 点击记录团队充值</a>"
+
+    var consumeStr = "<a href=\"http://your_IP/weixin/consume?weixinId=" + message.fromUsername + "\">2. 点击记录团队消费</a>"
+    var deleteStr = "<a href=\"http://your_IP/weixin/delete?weixinId=" + message.fromUsername + "\">3. 点击回退记录</a>"
+    var historyStr = "<a href=\"http://your_IP/weixin/history?weixinId=" + message.fromUsername + "\">4. 点击查询历史记录</a>"
+
+    var emptyStr = "          ";
+    var replyStr = "感谢你的关注！" + "\n"+ emptyStr + "\n" + refillStr + "\n"+ emptyStr + "\n" + consumeStr
+        + "\n"+ emptyStr + "\n" + deleteStr + "\n"+ emptyStr + "\n" + historyStr;
+    res.reply(replyStr);
+  }else if(message.MsgType == 'text'){
+    res.reply("fgfgfgfgf");
+  }
+}));
 //
 //app.use('/zhoufei',function(req,res,next){
 //  console.log("zhoufei");
